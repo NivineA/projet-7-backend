ed2c54726724fcc84d1a683575b941b51e0b89e2
NivineA <nivine.attoue@gmail.com>
2022-01-05 21:22:27 +0100

    ajout commentaire

--- README.md
+++ README.md
@@ -17,18 +17,21 @@ Un dossier notebook de preprocessing pour l'étude des données, l'entraînement
 
 Un fichier avec la configuration locale de l'API  api.py qui est le fichier Flask contenant la partie backend.
 
-un dossier venv_7 qui repr♪0sente l'environnement virtuel créé pour ce projet.
+un dossier venv_7 qui représente l'environnement virtuel créé pour ce projet.
 
 ### Accès a l'api
  online: https://projet-7-backend.herokuapp.com    
  local:  http://127.0.0.1:5000/
 
- Voici les endpoints de cette API:   
-
- load_data: pour la lecture du jeu de données     
- load_age_population: visualiser l'âge de tous les clients    
+ Voici les endpoints de cette API:     
+               
+ load_data: requête pour le chargement du jeu de données  
+ load_age_population: requête pour charger les âges de la population    
  load_revenus/population: visualiser le revenu de tous les clients    
- predict: pour prédire la classe du prêt pur un client (accepté ou refusé)     
- model_interpretation: visualise les shap_values du modèle d'un client    
- load_features: pour visualiser les features du modèle     
- load_voisins: visualise la liste des dossiers similaire à un certain client.    
+ predict: requête pour prédire le score d'un client sélectionné(accepté ou refusé)    
+ load_data_predict: requête pour charger les données avec le score prédit    
+ model_interpretation_shap: requête renvoyant les shap_values du modèle d'un client sélectionné     
+ load_data_shap: requête permet de charger les données utilisés pour le calcul des shap values     
+ load_features: requête illustrant les features du modèle    
+ load_feature_importance: requête permettant d'illustrer les importances des différents features du modèle    
+ load_voisins: visualise la liste des dossiers similaire à un certain client    
--- api.py
+++ api.py
@@ -18,22 +18,22 @@ data_final_test=pd.read_csv('notebook//test_final_sample.csv.zip')
 id_client = data_test_initial["SK_ID_CURR"][:1000].values
 id_client = pd.DataFrame(id_client)
 
-def generateMetrics():
-    description= "Bienvenue sur l'API du projet Implémentez un modèle de scoring:\n\nVoici les endpoints de cette API\n\n load_data: pour la lecture du jeu de données\n load_age_population: visualiser l'âge de tous les clients\n load_revenus/population: visualiser le revenu de tous les clients\n predict: pour prédire la classe du prêt pur un client (accepté ou refusé)\n model_interpretation: visualise les shap_values du modèle d'un client\n load_features: pour visualiser les features du modèle\n load_voisins: visualise la liste des dossiers similaire à un certain client"
+def generateMetrics():# page de bienvenue
+    description= "Bienvenue sur l'API du projet Implémentez un modèle de scoring:\n\nVoici les endpoints de cette API\n\n load_data: requête pour le chargement du jeu de données\n load_age_population: requête pour charger les âges de la population\n load_revenus/population: visualiser le revenu de tous les clients\n predict: requête pour prédire le score d'un client sélectionné(accepté ou refusé)\n load_data_predict: requête pour charger les données avec le score prédit\n model_interpretation_shap: requête renvoyant les shap_values du modèle d'un client sélectionné\n load_data_shap: requête permet de charger les données utilisés pour le calcul des shap values\n load_features: requête illustrant les features du modèle\n load_feature_importance: requête permettant d'illustrer les importances des différents features du modèle\n load_voisins: visualise la liste des dossiers similaire à un certain client"
     return(description)   
 
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["GET"])# afficher la description en format texte
 def home():
     response = make_response(generateMetrics(), 200)
     response.mimetype = "text/plain"
     return response
 
-@app.route("/load_data", methods=["GET"])
+@app.route("/load_data", methods=["GET"]) # charger les id d'un client sélectionné
 def load_data():
     return  id_client.to_json(orient='values')
 
 
-@app.route("/load_data_test", methods=["GET"])
+@app.route("/load_data_test", methods=["GET"]) # charger les données de tous les clients pour la visualisation graphique des distributions
 def load_data_test():
     new_data=data_final_test.drop(['Unnamed: 0', 'SK_ID_CURR'], axis=1)
     response = json.loads(new_data.to_json(orient='index'))
@@ -41,7 +41,7 @@ def load_data_test():
     return jsonify( response)
     
 
-@app.route("/infos_client", methods=["GET"])
+@app.route("/infos_client", methods=["GET"])# charger les données d'un client sélectionné
 def infos_client():
 
     id = request.args.get("id_client")
@@ -66,7 +66,7 @@ def infos_client():
 
     return jsonify( response)
 
-# Calcul des ages de la population pour le graphique
+# Calcul des ages de la population utilisé pour le graphique
 # situant l'age du client
 @app.route("/load_age_population", methods=["GET"])
 def load_age_population():
@@ -74,11 +74,15 @@ def load_age_population():
     df_age = round((data_test_initial["DAYS_BIRTH"] / -365), 2)
     return  df_age.to_json(orient='values')
 
+# Calcul des années d'emploi de la population utilisé pour le graphique
+
 @app.route("/load_days_employed_population", methods=["GET"])
 def load_days_employed_population():
   
     df_days_employed = round((data_test_initial["DAYS_EMPLOYED"] / -365), 2)
     return  df_days_employed.to_json(orient='values')
+
+# renvoie le genre de la population utilisé pour le graphique
 
 @app.route("/load_sex_population", methods=["GET"])
 def load_sex_population():
@@ -86,46 +90,54 @@ def load_sex_population():
     df_sex = (data_test_initial["CODE_GENDER"] )
     return  df_sex.to_json(orient='values')
 
+# renvoie le statut de famille de la population utilisé pour le graphique
 @app.route("/load_family_status_population", methods=["GET"])
 def load_family_status_population():
     
     df_family = (data_test_initial["NAME_FAMILY_STATUS"] )
     return  df_family.to_json(orient='values')
 
+#renvoie le niveau d'éducation de la population utilisé pour le graphique
 @app.route("/load_education_population", methods=["GET"])
 def load_education_population():
     
     df_education = (data_test_initial["NAME_EDUCATION_TYPE"] )
     return  df_education.to_json(orient='values')
 
+#renvoie le type de revenu de la population utilisé pour le graphique
 @app.route("/load_income_type_population", methods=["GET"])
 def load_income_type_population():
     df_income = (data_test_initial["NAME_INCOME_TYPE"] )
     return  df_income.to_json(orient='values')
 
+#renvoie le revenu de la population utilisé pour le graphique
 @app.route("/load_revenus_population", methods=["GET"])
 def load_revenus_population():
     
     df_revenus = data_test_initial["AMT_INCOME_TOTAL"] 
     return  df_revenus.to_json(orient='values')
 
+#renvoie le type de contrat de la population utilisé pour le graphique
 @app.route("/load_contract_population", methods=["GET"])
 def load_contract_population():
     df_contract = (data_test_initial["NAME_CONTRACT_TYPE"] )
     return  df_contract.to_json(orient='values')
 
+#renvoie le montant du crédit  de la population utilisé pour le graphique
 @app.route("/load_credit_population", methods=["GET"])
 def load_credit_population():
     
     df_credit = data_test_initial["AMT_CREDIT"] 
     return  df_credit.to_json(orient='values')
 
+#renvoie le montant des annuités  de la population utilisé pour le graphique
 @app.route("/load_annuity_population", methods=["GET"])
 def load_annuity_population():
     
     df_annuity = data_test_initial["AMT_ANNUITY"] 
     return  df_annuity.to_json(orient='values')
 
+#prédit le score de faillite d'un client sélectionné
 @app.route("/predict", methods=["GET"])
 def predict():
     
@@ -143,6 +155,7 @@ def predict():
     print(prediction)
     return jsonify(prediction)
 
+#permet de charger les données  des client incluant  le score calculé
 @app.route("/load_data_predict", methods=["GET"])
 def load_data_predict():
     score=[]
@@ -164,12 +177,12 @@ def load_data_predict():
     response = json.loads(test.to_json(orient='index'))
     return jsonify( response)
 
+# permet de calculer les shap values du modèle pour un client sélectionné
 @app.route("/model_interpretation_shap", methods=["GET"])
 def model_interpretation_shap():
     id = request.args.get("id_client")
     idx=data_final_test.loc[data_final_test["SK_ID_CURR"] ==float (id)].index
     new_data_test=data_final_test.drop(['Unnamed: 0', 'SK_ID_CURR'], axis=1)
-    new_data_train=data_train.drop(['Unnamed: 0', 'TARGET'], axis=1)
     data_client = new_data_test.iloc[idx]
     shap.initjs()
     model_explainer=shap.TreeExplainer(model, feature_perturbation="tree_path_dependent" )
@@ -177,36 +190,22 @@ def model_interpretation_shap():
     shap_values=np.asarray(shap_values)
     shap_values=shap_values.tolist()
     return  jsonify (shap_values)
-   
 
-@app.route("/model_interpretation_expected", methods=["GET"])
-def model_interpretation_expected():
-    
-    id = request.args.get("id_client")
-    idx=data_final_test.loc[data_final_test["SK_ID_CURR"] ==float (id)].index
-    new_data_test=data_final_test.drop(['Unnamed: 0', 'SK_ID_CURR'], axis=1)
-    new_data_train=data_train.drop(['Unnamed: 0', 'TARGET'], axis=1)
-    data_client = new_data_test.iloc[idx]
-    shap.initjs()
-    model_explainer=shap.TreeExplainer(model)
-    shap_values = model_explainer.shap_values(data_client, check_additivity=False)
-    expected_values = model_explainer.expected_value
-    return  jsonify (expected_values)
-
+# calcul de l'importance des features
 @app.route("/load_feature_importance", methods=["GET"])
 def load_feature_importance():
     features_importance = model.feature_importances_
     features_importance=features_importance.tolist()
     return jsonify(features_importance)
 
+# charger les features du modèle
 @app.route("/load_features", methods=["GET"])
 def load_features():
     new_data_test=data_final_test.drop(['Unnamed: 0', 'SK_ID_CURR'], axis=1)
     features=new_data_test.columns.values.tolist() 
     return jsonify (features)
 
-
-
+# charger les données utilisé par shap après la sélection finale des features
 @app.route("/load_data_shap", methods=["GET"])
 def load_data_shap():
     id = request.args.get("id_client")
@@ -216,6 +215,7 @@ def load_data_shap():
     response = json.loads(data_client.to_json(orient='index'))
     return jsonify (response)
 
+# afficher les 10 dossiers similaires au client sélectionné
 @app.route("/load_voisins", methods=["GET"])
 def load_voisins():
     
